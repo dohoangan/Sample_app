22871db5cc8a185943728ee72e365194b167c7c1
An D. Hoang <an@izproperty.com>
2012-09-20 01:35:15 +0700

    Improve page title

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -5,22 +5,29 @@ describe "Static pages" do
 	describe "Home Page" do
 		it "should have the content 'Sample App'" do
 			visit '/static_pages/home'
-			page.should have_content('Sample App')
+			page.should have_selector('h1', 		:text => 'Sample App')
+			page.should have_selector('title', 	
+											:text => "Ruby On Rails Sample App")
 		end
 	end
 
 	describe "Help Page" do
 		it "should have the content 'Help Page'" do
 			visit '/static_pages/help'
-			page.should have_content('Help Page')
+			page.should have_selector('h1', text: 'Help Page')
+			page.should have_selector('title', 
+											text: 'Ruby On Rails Sample App | Help')
 		end		
 	end
 
-	describe "About Page" do
+	describe "About Us" do
 		it "should have the content 'About Page'" do
 			visit '/static_pages/about'
-			page.should have_content('About Page')
+			page.should have_selector('h1', :text => 'About Us')
+			page.should have_selector('title', 
+											:text => 'Ruby On Rails Sample App | About us')
 		end
 	end
 
 end
+
