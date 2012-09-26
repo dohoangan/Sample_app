08bfdbe5c4c443a8d744946df2cec175ce84572a
An D. Hoang <an@izproperty.com>
2012-09-27 00:21:12 +0700

    minor change

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,7 +1,7 @@
 module SessionsHelper
 	def sign_in(user)
     cookies.permanent[:remember_token] = user.remember_token
-    self.current_user = user
+    current_user = user
   end
 
   def signed_in?
@@ -21,7 +21,7 @@ module SessionsHelper
   end
 
   def sign_out
-    self.current_user = nil
+    current_user = nil
     cookies.delete(:remember_token)
   end
 end
